35e588b508f083b44058b73e179ca27724eb3edd
ZNurr <z.nurciftci@gmail.com>
2022-05-11 13:37:25 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class MyDictionary<TKey, TValue> 
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];  
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempArray1 = keys;
+            keys = new TKey[keys.Length + 1];
+
+            TValue[] tempArray2 = values;
+            values = new TValue[values.Length + 1];
+
+
+            for (int i = 0; i < tempArray1.Length; i++)
+            {
+                keys[i] = tempArray1[i];
+                values[i] = tempArray2[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public int Length
+        {
+            get { return keys.Length; }
+        }
+
+        public TKey[] key
+        {
+            get { return keys; }
+        }
+        public TValue[] value
+        {
+            get { return values; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using MyDictionary;
+
+
+    MyDictionary<int, string> persons = new MyDictionary<int, string>();
+    persons.Add(1, "Nur");
+    persons.Add(2, "Yakup");
+    persons.Add(3, "Yasir");
+
+    foreach (var person in persons.value)
+    {
+        Console.WriteLine(person);
+    }
+    foreach (var person in persons.key)
+    {
+        Console.WriteLine(person);
+    }
+
+    Console.WriteLine("Dizinin Uzunluğu: " + persons.Length);
